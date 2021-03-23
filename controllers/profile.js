5f3914e0b23a5e4eaa222830b7237ad84158d15f
ChristopheBouriel <christophe.bouriel@gmail.com>
2021-03-23 15:14:48 +0100

    correction query dans profile colonne likes

--- controllers/profile.js
+++ controllers/profile.js
@@ -18,7 +18,7 @@ exports.seeProfile = (req, res, next) => {
       const checkUserId = decodedToken.userId;
       if (checkUserId) {
         const userInfos = result;
-        connexion.query(`SELECT id, date_publication, title, content, likes, numberComments, userName, modified, date_modif, moderated FROM publications WHERE userName = ?`, [req.params.userName], (error, result)=>{
+        connexion.query(`SELECT id, date_publication, title, content, likeUsernames, numberComments, userName, modified, date_modif, moderated FROM publications WHERE userName = ?`, [req.params.userName], (error, result)=>{
             if(error) {res.status(500).send(error.sqlMessage)}
             else{
               const response = {userInfos, result}; 
